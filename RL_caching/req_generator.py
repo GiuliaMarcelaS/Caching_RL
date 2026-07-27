fa689d4f5725661696a9123e458e99ab9cbc778e
Natalie Cunha <122024206+gioocunha@users.noreply.github.com>
2026-07-27 19:26:25 +0200

    Revert "Estruturaçao dos experimentos, com o dataset e sem"

--- RL_caching/req_generator.py
+++ RL_caching/req_generator.py
@@ -1,15 +1,9 @@
 import numpy as np
 
-def Uniform(F, seed=None):
+def Uniform(F):
     p = np.ones(F)/F
-    if seed is not None:
-        pass
     return p
 
-def zipf(alpha, num_files, seed=None):
+def zipf(alpha, num_files):
     p = np.array([(i+1)**-alpha for i in range(num_files)])
-    p = p/sum(p)
-    if seed is not None:
-        rng = np.random.default_rng(seed)
-        p = rng.permutation(p)
-    return p
+    return p/sum(p)
